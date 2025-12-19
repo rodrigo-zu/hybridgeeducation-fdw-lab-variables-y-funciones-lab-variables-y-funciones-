e1cbb749ede955ab928b1744b08b5718ac3b2c10
rodrigo-zu <rodrigo2407099@hybridge.education>
2025-12-19 12:45:22 -0600

    Update ejercicio06.js

--- ejercicio06.js
+++ ejercicio06.js
@@ -2,7 +2,10 @@
 // Declara una función llamada "celsiusAFahrenheit" que acepte un parámetro "celsius" y retorne la conversión a Fahrenheit
 
 //escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "celsiusAFahrenheit"
-
+function celsiusAFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+let o_fahrenheit = celsiusAFahrenheit(50); 
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
