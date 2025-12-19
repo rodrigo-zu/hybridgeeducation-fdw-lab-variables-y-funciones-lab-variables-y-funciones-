e8ef098ce1ed9af1aa8516b1219da0f41539c2f7
rodrigo-zu <rodrigo2407099@hybridge.education>
2025-12-19 12:24:00 -0600

    Update ejercicio02.js

--- ejercicio02.js
+++ ejercicio02.js
@@ -2,8 +2,12 @@
 // Declara una función llamada "obtenerPI" que retorne el valor 3.1416
 
 //define tu función aquí
+function obtenerPI() {
+    const pi = 3.1416;
+    return pi;
+}
 
-
+let o_pi = obtenerPI();
 // No modificar
 // Exporta la función para que pueda ser utilizada en otros archivos
 // Como ves, tu función debe tener el nombre obtenerPI
