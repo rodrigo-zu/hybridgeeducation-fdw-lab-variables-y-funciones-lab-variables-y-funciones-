6645103ceb8ac910704b84b41633a73454b3b957
rodrigo-zu <rodrigo2407099@hybridge.education>
2025-12-19 12:29:17 -0600

    Update ejercicio03.js

--- ejercicio03.js
+++ ejercicio03.js
@@ -2,7 +2,10 @@
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
 
 //Escribe tu función aquí
-
+function sumar(p1, p2) {
+    return p1 + p2;
+}
+ let o_suma = sumar(10, 20);
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
