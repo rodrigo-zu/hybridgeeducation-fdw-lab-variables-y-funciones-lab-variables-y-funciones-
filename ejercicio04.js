fcff4165799dd790188a4c648108ee51b0bf7c43
rodrigo-zu <rodrigo2407099@hybridge.education>
2025-12-19 12:33:59 -0600

    Update ejercicio04.js

--- ejercicio04.js
+++ ejercicio04.js
@@ -2,7 +2,11 @@
 // Declara una función llamada "saludar" que acepte un parámetro "nombre" y retorne un mensaje de saludo con la estructura "Hola, nombre!".
 
 // Escribe tu función aquí. El nombre exacto debe ser saludar
+function saludar(nombre) {
+    return "Hola, " + nombre + "!";
+}
 
+let o_saludar = saludar("Rodrigo")
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     saludar
