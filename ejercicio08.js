f4422bfba1adfb6456f9bca1f35a7d4fb8fb002b
rodrigo-zu <rodrigo2407099@hybridge.education>
2025-12-19 12:56:14 -0600

    Update ejercicio08.js

--- ejercicio08.js
+++ ejercicio08.js
@@ -6,7 +6,11 @@
 // concatenarCadenas("Frase", "de prueba") => "Frasede prueba"
 
 //escribe aquí tu función. Recuerda que el nombre de la función debe ser exactamente "concatenarCadenas"
+function concatenarCadenas (cadena1, cadena2) {
+    return cadena1 + cadena2;
+}
 
+let o_cadena = concatenarCadenas("Hola ", "Mundo.")
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     concatenarCadenas
