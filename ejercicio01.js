437ff29b4b68c7dc05c56314d6e3ea8105c5b56f
rodrigo-zu <rodrigo2407099@hybridge.education>
2025-12-19 12:19:54 -0600

    Update ejercicio01.js

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,9 +2,10 @@
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    let nombre = "Rodrigo"; 
+    return nombre
 }
-
+let o_nombre = obetenerNombre();
 // No modificar.
 // Exporta la función para que pueda ser utilizada en otros archivos
 module.exports = {
